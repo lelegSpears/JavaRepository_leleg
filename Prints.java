100be90874b4f626b3b657a0ad7566b2043aae88
leleg <lelegsoares@gmail.com>
2025-11-06 14:37:57 -0300

    Update Prints.java
     Adicionando jeito alternativo.

--- Prints.java
+++ Prints.java
@@ -9,7 +9,9 @@ public class Main {
 		double salario = 5.300;
 		
 		
-		System.out.printf("%s tem %d anos e recebe %.2f de salario", nome, idade, salario);
+		System.out.printf("%s tem %d anos e recebe %.2f de salario.\n", nome, idade, salario);
+		
+		System.out.println(nome + " tem " + idade + " anos e recebe " + String.format("%.2f", salario) + " de salario.");
 	}
 
 }
