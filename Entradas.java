059fb17ba144798f65f8297884e83b31b302587b
leleg <lelegsoares@gmail.com>
2025-11-08 00:45:32 -0300

    Update Entradas.java

--- Entradas.java
+++ Entradas.java
@@ -10,24 +10,25 @@ public class Entradas {
 		
 		System.out.println(" Digite: ");
 		String Palavra = sc.next();		// Utiliza o Scanner criado(sc) usando next(primeira palavra somente)
-		System.out.printf(" Você digitou: %s\n", Palavra);
+		System.out.printf(" Você digitou: %s\n\n", Palavra);
 		sc.nextLine();	 // Limpa caso digite mais de uma palavra no Next
 		
 		System.out.println(" Digite: ");
 		String Frase = sc.nextLine();		// Utiliza o Scanner criado(sc) usando nextLine(Frase inteira)
-		System.out.printf(" Você digitou: %s\n", Frase);
+		System.out.printf(" Você digitou: %s\n\n", Frase);
 		
 		System.out.println(" Digite: ");
 		int Numero = sc.nextInt();		// Utiliza o Scanner criado(sc) usando nextInt(Inteiro)
-		System.out.printf(" Você digitou: %d\n", Numero);
+		System.out.printf(" Você digitou: %d\n\n", Numero);
 		sc.nextLine();
 		
 		System.out.println(" Digite: ");
 		Double NumQuebrado = sc.nextDouble();		// Utiliza o Scanner criado(sc) usando nextDouble(Numero Decimal)
-		System.out.printf(" Você digitou: %.2f\n", NumQuebrado);
+		System.out.printf(" Você digitou: %.2f\n\n", NumQuebrado);
 		sc.nextLine();
 		
 		sc.close();		// Fecha o Scan
 	}
 
 }
+
