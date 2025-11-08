ffdfd299c55882ff89740e9c51e631b8a8fd7e9c
leleg <lelegsoares@gmail.com>
2025-11-08 00:38:43 -0300

    Entradas Scanner System.in
     Adicionando metodos de Entrada:
    Scanner sc = new Scanner(System.in)
    sc.next(), sc.nextLine(), sc.nextInt e sc.nextDouble()
    sc.close()

---
+++ Entradas.java
@@ -0,0 +1,33 @@
+import java.util.Locale;
+import java.util.Scanner; // Biblioteca de Entrada
+
+public class Entradas {
+
+	public static void main(String[] args) {
+		Locale.setDefault(Locale.US);
+		
+		Scanner sc = new Scanner(System.in);	// Cria o Scanner "sc" e recebe entrada do teclado(System.in)
+		
+		System.out.println(" Digite: ");
+		String Palavra = sc.next();		// Utiliza o Scanner criado(sc) usando next(primeira palavra somente)
+		System.out.printf(" Você digitou: %s\n", Palavra);
+		sc.nextLine();	 // Limpa caso digite mais de uma palavra no Next
+		
+		System.out.println(" Digite: ");
+		String Frase = sc.nextLine();		// Utiliza o Scanner criado(sc) usando nextLine(Frase inteira)
+		System.out.printf(" Você digitou: %s\n", Frase);
+		
+		System.out.println(" Digite: ");
+		int Numero = sc.nextInt();		// Utiliza o Scanner criado(sc) usando nextInt(Inteiro)
+		System.out.printf(" Você digitou: %d\n", Numero);
+		sc.nextLine();
+		
+		System.out.println(" Digite: ");
+		Double NumQuebrado = sc.nextDouble();		// Utiliza o Scanner criado(sc) usando nextDouble(Numero Decimal)
+		System.out.printf(" Você digitou: %.2f\n", NumQuebrado);
+		sc.nextLine();
+		
+		sc.close();		// Fecha o Scan
+	}
+
+}
