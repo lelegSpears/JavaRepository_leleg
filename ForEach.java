ae8b6af074b84af096808f0ab3f3fd4300efe89a
leleg <lelegsoares@gmail.com>
2025-11-14 16:22:58 -0300

    ForEach
     Forma alternativa a (i=0; i>n; i++);

---
+++ ForEach.java
@@ -0,0 +1,10 @@
+public class ForEach {
+
+	public static void main(String[] args) {
+		
+		String[] vetor = new String[] {"Lukaskas", "Enzo", "Lucko"};
+		
+		for (String Apelido : vetor) { // Percorre todo o vetor dando o apelido "Apelido".
+		System.out.println(Apelido);
+	}
+}}
